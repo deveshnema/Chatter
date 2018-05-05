b3df783e8e6daced996e11b8156ae2edec2f4ec5
Devesh Nema <deveshnema@deveshs-macbook-air.local>
2018-05-04 21:39:27 -0700

    Created FBSDKLoginButton programmatically and implemented logging in with facebook

--- Chatter/ViewController.swift
+++ Chatter/ViewController.swift
@@ -7,18 +7,45 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    //MARK:- Outlets
+
+    //MARK:- Delegate methods
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        print("Successfully logged in with FB")
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields" : "id, name, email"]).start {
+            (connection, result, err) in
+            if err != nil {
+                print("Graph request failed")
+                return
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Logged out of FB")
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Create the FB login button
+        let fbLoginButton = FBSDKLoginButton()
+        view.addSubview(fbLoginButton)
+        fbLoginButton.frame = CGRect(x: 16, y: 200, width: view.frame.width - 32, height: 50)
+        fbLoginButton.delegate = self
+        fbLoginButton.readPermissions = ["email", "public_profile"]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
